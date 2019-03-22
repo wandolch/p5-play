2dcd2b409952f9111e04e36b418b8b2204570c70
Ivan Nelyubov <nelyubovi@ae.com>
2019-03-22 15:00:18 +0100

    win

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 import * as p5 from 'p5';
 
 let sketch = function () { 
-    this.setup = () =>{
-        this.createCanvas(window.innerWidth,window.innerHeight);
-    }
-    this.draw = () => {
-      if (this.mouseIsPressed) {
-        this.fill(0);
-      } else {
-        this.fill(255);
+  let winXP, win2000, win98;
+  let mode = 'win98';
+
+  this.preload = () => {
+    winXP = this.loadImage('https://upload.wikimedia.org/wikipedia/de/9/9e/Windows-Explorer_Windows_XP.png');
+    win2000 = this.loadImage('./assets/win2000.png');
+    win98 = this.loadImage('./assets/win98.png');
+  }
+
+  this.setup = () =>{
+      this.createCanvas(window.innerWidth,window.innerHeight);
+  }
+
+  this.draw = () => {
+    if (this.mouseIsPressed && this.mouseX + this.mouseY !== 0) {
+      switch (mode) {
+        case 'winXP':
+          this.image(winXP, this.mouseX-150, this.mouseY-10);
+          break;
+        case 'win2000':
+          this.image(win2000, this.mouseX-150, this.mouseY-10);
+          break;
+        case 'win98':
+          this.image(win98, this.mouseX-150, this.mouseY-10);
+          break;
       }
-      this.ellipse(this.mouseX, this.mouseY, 80, 80);
     }
+  }
 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
+const path = require('path');
+
 module.exports = {
     entry: "./main.js",
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+        chunkFilename: '[name].chunk.js'
     },
     module: {
         rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules)/,
-            use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/fonts/'
+          
+                    }
+                }]
             }
-            }
-        }
         ]
     }
 };
